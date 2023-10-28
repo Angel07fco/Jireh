b593452fa17775e9649b3b81204c72ca39ebd071
Angel Fco <angel02fco@gmail.com>
2023-10-28 16:59:07 -0600

    add files

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavbarAdmin from '../../components/NavbarAdmin';
+import '../../assets/styles/Admin/Admin.css'
 
 function Admin() {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div>
+    <div className='admin-container'>
 
       <NavbarAdmin />
+
+      <div className='contenidoA'>
+
+        <h1>Clinica Veterinaria JIREH</h1>
+
+        <div className='esloganA'>
+          <div className='imageE'>
+            <div className='imageSeleccionada'>
+              {selectedImage && (
+                <img src={selectedImage} alt="Imagen seleccionada" />
+              )}
+            </div>
+            <div className='inputImage'>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                id="imageInput"
+                style={{ display: 'none' }}
+              />
+              <label htmlFor="imageInput" className="custom-file-input">
+                Seleccionar una imagen
+              </label>
+            </div>
+          </div>
+          <div className='textoE'>
+            <h2>Eslogan o frase llamativa</h2>
+            <div className='textoContenidoE'>
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+      </div>
 
     </div>
   )
