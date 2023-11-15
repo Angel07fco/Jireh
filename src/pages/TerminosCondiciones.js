df3b214d28f0ff0232bc38d35c549c14f61b9327
Angel Fco <angel02fco@gmail.com>
2023-11-15 03:27:31 -0600

    add files

--- src/pages/TerminosCondiciones.js
+++ src/pages/TerminosCondiciones.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
 
 function TerminosCondiciones() {
   return (
-    <div>TerminosCondiciones</div>
+    <div className='terminos'>
+      
+      <Navbar />
+
+      <section className='section-terminos'>
+        <h1 className='title'>Terminos y Condiciones</h1>
+      </section>
+
+    </div>
   )
 }
 
