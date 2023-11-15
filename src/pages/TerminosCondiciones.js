6a360cb0c22215863a9b660b7cf3c015740aac4b
Angel Fco <angel02fco@gmail.com>
2023-11-15 12:41:37 -0600

    terminos y condiciones

--- src/pages/TerminosCondiciones.js
+++ src/pages/TerminosCondiciones.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import "../assets/styles/Terminos.css";
+import Footer from '../components/Footer';
 
 function TerminosCondiciones() {
   return (
@@ -9,7 +11,155 @@ function TerminosCondiciones() {
 
       <section className='section-terminos'>
         <h1 className='title'>Terminos y Condiciones</h1>
+        <h5>Los siguientes términos y condiciones (los "Términos y Condiciones") rigen el uso que usted le dé al sitio web Jireh (el "sitio web") y a cualquiera de los contenidos disponibles por o a través de este Sitio Web, incluyendo cualquier contenido derivado del mismo. Jireh ha puesto a su disposición el Sitio Web. Podemos cambiar los Términos y Condiciones de Uso, sin embargo estos Términos y Condiciones puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios. AL USAR EL SITIO WEB, USTED ACEPTA Y ESTÁ DE ACUERDO CON ESTOS TÉRMINOS Y CONDICIONES EN LO QUE SE REFIERE AL USO DEL SITIO WEB. Si usted no está de acuerdo con estos Términos y Condiciones, no puede tener acceso al mismo ni usar el Sitio Web de ninguna otra manera. </h5>
+        <h5>Jireh pone a su disposición la siguiente información:</h5>
+        <ul>
+          <li><strong>Titular:</strong> Clínica Veterinaria Jireh</li>
+          <li><strong>Ubicación:</strong> Bulevar Adolfo López S/N Colonia Aviación Civil</li>
+          <li><strong>Teléfono de contacto:</strong> 77 1162 0008</li>
+        </ul>
+      
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Jireh</h3>
+          <h5>La Clínica Veterinaria Jireh pone a su disposición sus diversos servicios veterinarios (Urgencias, Servicio a domicilio, Vacunación, etc.), a sí mismo una amplia variedad de productos (Accesorios, Medicamentos, Tratamientos, etc.). </h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Asignación de riesgos, responsabilidades y descargos de responsabilidad</h3>
+          <h5>El Titular declina cualquier responsabilidad en caso de que existan interrupciones o un mal funcionamiento de los Servicios o contenidos ofrecidos en el sitio web, cualquiera que sea su causa. Asimismo, el Titular no se hace responsable por caídas de la red, pérdidas de negocio a consecuencia de dichas caídas, suspensiones temporales de fluido eléctrico o cualquier otro tipo de daño indirecto que te pueda ser causado por causas ajenas al Titular.</h5>
+          <h5><strong>Responsabilidades del Usuario (Propietario de la Mascota):</strong></h5>
+          <h5>El propietario o titular (en su caso) de la mascota se compromete a proporcionar información precisa sobre la salud y el historial médico del animal al equipo de la Clínica Veterinaria Jireh.</h5>
+          <h5>Asegurar el bienestar general de la mascota y seguir las recomendaciones y pautas proporcionadas por el personal veterinario.</h5>
+          <h5><strong>Riesgos Asociados con el Tratamiento Veterinario:</strong></h5>
+          <h5>Reconocimiento de que ciertos procedimientos médicos o tratamientos veterinarios conllevan riesgos inherentes, y el propietario de la mascota asume esos riesgos.</h5>
+          <h5><strong>Descargos de Responsabilidad:</strong></h5>
+          <h5>La Clínica Veterinaria Jireh no se hace responsable de la pérdida o daño de objetos personales dejados en las instalaciones.</h5>
+          <h5>Exención de responsabilidad por eventos imprevistos durante la estancia de la mascota en la clínica.</h5>
+          <h5><strong>Indemnización:</strong></h5>
+          <h5>Compromiso del propietario de indemnizar a la Clínica Veterinaria Jireh por reclamaciones o pérdidas resultantes de acciones indebidas o incumplimientos de los términos acordados.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Garantías (Solo aplica a productos)</h3>
+          <h5><strong>Tienda física</strong></h5>
+          <h5>Para poder realizar cambios de productos adquiridos deberá acudir a la Clínica Veterinaria Jireh, en debido caso deberá solicitar una nota o ticket de compra para poder hacer valida una garantía. Además de presentar su nota o ticket deberá llevar el producto en perfecto estado con todas sus etiquetas y embalajes originales, en caso de algún medicamento, tratamiento o producto de consumo de sus mascota deberá presentarlo tal y como fue su primer consumo o algún defecto encontrado. De haber modificado el precio del producto y estar dentro de un plazo de 15 días, se considerara el precio a la fecha de compra que figure la nota o el ticket de compra.</h5>
+          <h5><strong>Venta Online</strong></h5>
+          <h5>El plazo para cambios o devoluciones tiene un máximo de 5 días desde la compra o recepción del producto, es muy importante que al momento de la recepción de la compra revise el buen estado del producto antes de firmar el recibo de entrega o confirmar en la página que recibió su producto. Su nombre, firma y código de seguridad al momento de la entrega será muestra de conformidad. En caso de que el cliente no se encuentre conforme con el producto adquirido, podrá cambiarlo por cualquier otro producto de igual valor o varios productos que equivalgan su valor o en su caso abonando la diferencia dado que el nuevo valor supere el original.  No se aceptan reclamos por daños estéticos y/o faltantes una vez firmado el recibo de entrega o que haya confirmado en el sitio web. De haber modificado el precio del producto y estar dentro de un plazo de 15 días, se considerara el precio a la fecha de compra que figure la nota o el ticket de compra, en este caso en su pedido el sitio web.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Derecho de desistimiento </h3>
+          <h5><strong>Servicios</strong></h5>
+          <h5>El propietario o titular (en su caso) tiene derecho de desistir de los servicios contratados con la Clínica Veterinaria Jireh dentro un plazo de 1 días hábiles antes de acudir a la cita, una condición es la necesidad de justificar dicho desistimiento. </h5>
+          <h5>En dado caso de no presentarse a dicha cita, su cuenta quedara inhabilitada y no podrá realizar citas dentro de un periodo de 4 meses como mínimo. Para solucionar este inconveniente deberá presentarse en la Clínica Veterinaria Jireh. </h5>
+          <h5><strong>Productos</strong></h5>
+          <h5>El propietario o titular (en su caso) tiene derecho de desistir sobre la compra realizada de algún producto, si dicho producto se entrega en zonas cercanas a la Clínica Veterinaria Jireh tiene un plazo de 1 días hábiles posteriores a la compra, sin necesidad de justificación.</h5>
+          <h5>Si el producto se entrega en zonas lejanas a la Clínica Veterinaria Jireh tiene un plazo de 2 días hábiles posteriores a la compra, sin necesidad de justificación. </h5>
+          <h5>La Clínica Veterinaria Jireh reembolsara el pago correspondiente recibido por el propietario. </h5>
+          <h5>En el caso de que el propietario decida desistir de la recepción del producto en su domicilio o se niegue a recibirlo, se le solicitará que confirme esta decisión a través del sitio web y firme el recibo de entrega correspondiente. Al hacerlo, la Clínica Veterinaria Jireh comprende y acepta que este acto lo deslinda de cualquier posible problemática asociada a la entrega.</h5>
+          <h5>Es importante destacar que, al confirmar la recepción mediante la firma del recibo o en el sitio web, el propietario reconoce que el pago realizado no está sujeto a devolución. En caso de surgir cualquier inconveniente de resolver o una situación particular, se le pide acudir a la Clínica Veterinaria Jireh dentro un plazo de 15 días al haber confirmado o recibido la entrega.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Condiciones de compra o solicitar un servicio</h3>
+          <h5>La Clínica Veterinaria Jireh tiene un gran alcance demográfico de clientes, por lo que el único requisito para realizar alguna compra o solicitar un servicio este deberá encontrase dentro del alcance demográfico que señale el sitio web y a su vez haber proporcionado toda la información necesaria para su validación, en el caso de propietarios que no radiquen en la ciudad de Huejutla de Reyes, Hgo. Para poder solicitar una cita o una compra este deberá haber sido atendido previamente en la Clínica Veterinaria Jireh.</h5>
+          <h5>En el caso de solicitar un servicio en su caso una cita, el propietario está comprometido en proporcionar toda información que sea relevante a la salud de su mascota.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Reembolsos, cambios o cese del servicio o producto</h3>
+          <h5><strong>Reembolsos (solo aplica a productos)</strong></h5>
+          <h5>Para hacer valido el reembolso de algún producto las razones válidas serian:</h5>
+          <ul>
+            <li>Defecto del producto</li>
+            <li>Insatisfacción</li>
+            <li>Entrega incorrecta</li>
+            <li>Problemas de calidad.</li>
+          </ul>
+          <h5>Para hacer valido el reembolso deberá seguir lo que se estable en <strong>Garantías</strong>.</h5>
+          <h5><strong>Cambios</strong></h5>
+          <h5>El propietario tiene derecho a rectificar ya sea la compra de un producto o la solicitud de algún servicio, para ello deberá tomar en consideración lo que se establece en Garantías, donde especifica motivos, días hábiles y consideración a tomar, esto aplica también a servicios.</h5>
+          <h5><strong>Cese</strong></h5>
+          <h5>Jireh puede optar por dejar de ofrecer algún servicio o producto en particular. Algunas de las situaciones que puede conllevar a esto serian:</h5>
+          <ul>
+            <li>Obsolescencia del producto</li>
+            <li>Problemas de producción o suministro</li>
+            <li>Cumplimiento de normativas o requisitos legales</li>
+            <li>Problemas financieros</li>
+            <li>Avances tecnológicos</li>
+            <li>Cambios en la oferta de servicios o productos</li>
+          </ul>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Métodos de pago</h3>
+          <h5>La Clínica Veterinaria Jireh acepta estos métodos de pago:</h5>
+          <h5><strong>En efectivo</strong></h5>
+          <h5>Los propietarios entregan dinero en efectivo a efectuar su compra o al término de un servicio.</h5>
+          <h5><strong>Tarjeta de Crédito</strong></h5>
+          <h5>Los propietarios pueden pagar utilizando tarjetas de crédito, esto les permite realizar compras a través de nuestra tienda en línea.</h5>
+          <h5><strong>Tarjeta de Débito</strong></h5>
+          <h5>Similar a las tarjetas de crédito, pero los fondos se deducen directamente de la cuenta bancaria del propietario.</h5>
+          <h5><strong>Transferencias Bancarias</strong></h5>
+          <h5>Los propietarios pueden realizar transferencias electrónicas desde su cuenta bancaria a la cuenta del dueño de la Clínica Veterinaria Jireh.</h5>
+          <h5><strong>Otros</strong></h5>
+          <h5>En su caso el cliente puede usar algún otro método de pago, esto queda a consideración entre el dueño de la Clínica Veterinaria Jireh y el propietario.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Propiedad intelectual</h3>
+          <h5>Todos los derechos están reservados.</h5>
+          <h5>Todo acceso a este sitio web está sujeto a las siguientes condiciones: la reproducción, almacenaje permanente y la difusión de los contenidos o cualquier otro uso que tenga finalidad pública o comercial queda expresamente prohibida sin el consentimiento previo expreso y por escrito del Titular.</h5>
+          <h5><strong>Derechos de Autor</strong></h5>
+          <h5>Todos los contenidos originales presentes en nuestro sitio web, incluyendo textos, imágenes, gráficos y otros materiales, están protegidos por derechos de autor. La Clínica Veterinaria Jireh es la titular exclusiva de estos derechos, y cualquier uso no autorizado está estrictamente prohibido.</h5>
+          <h5><strong>Marcas Comerciales</strong></h5>
+          <h5>Los nombres, logotipos y cualquier otro signo distintivo asociado con la Clínica Veterinaria Jireh son marcas comerciales registradas. Su uso no autorizado para productos o servicios similares está prohibido y será objeto de acciones legales.</h5>
+          <h5><strong>Patentes</strong></h5>
+          <h5>Cualquier innovación única o método exclusivo desarrollado por la Clínica Veterinaria Jireh está protegido por patentes. Estos derechos exclusivos garantizan la propiedad y el control sobre el uso de estas innovaciones.</h5>
+          <h5><strong>Diseños Industriales</strong></h5>
+          <h5>La apariencia estética de nuestros servicios, instalaciones o productos está protegida como diseño industrial. La reproducción no autorizada de estos diseños está estrictamente prohibida.</h5>
+          <h5><strong>Derechos de Publicidad</strong></h5>
+          <h5>La Clínica Veterinaria Jireh se reserva el derecho de utilizar imágenes y nombres asociados con testimonios, eventos o promociones para fines publicitarios. Cualquier uso no autorizado de estos derechos está prohibido.</h5>
+          <h5><strong>Licencias de Uso</strong></h5>
+          <h5>Los propietarios pueden acceder y utilizar los contenidos del sitio web únicamente para fines personales y no comerciales. Cualquier otro uso, incluyendo la reproducción o distribución, requiere autorización expresa de la Clínica Veterinaria Jireh.</h5>
+          <h5><strong>Cumplimiento Legal</strong></h5>
+          <h5>Nos reservamos el derecho de emprender acciones legales en caso de infracción de nuestros derechos de propiedad intelectual e industrial.</h5>
+          <h5><strong>Notificaciones de Violación de Derechos</strong></h5>
+          <h5>Si los propietarios o terceros observan posibles violaciones de nuestros derechos, les animamos a notificarnos para abordar de manera efectiva cualquier problema relacionado con la propiedad intelectual.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Condiciones Adicionales Aplicables</h3>
+          <h5><strong>Promociones Especiales</strong></h5>
+          <h5>Durante períodos de promociones o descuentos especiales, pueden aplicarse condiciones específicas, como fechas de validez, restricciones de uso o términos particulares de la oferta.</h5>
+          <h5><strong>Fidelidad</strong></h5>
+          <h5>Para los propietarios que tengas cierto porcentaje de fidelidad, existen condiciones adicionales relacionadas con acumulación de puntos, canje de beneficios y descuentos.</h5>
+          <h5><strong>Servicios Específicos</strong></h5>
+          <h5>Algunos servicios veterinarios pueden tener condiciones adicionales según la naturaleza del tratamiento o la intervención médica. Estas condiciones se establecen para garantizar la seguridad y el bienestar de los animales atendidos.</h5>
+          <h5><strong>Eventos y Campañas Especiales</strong></h5>
+          <h5>En el caso de eventos, campañas de salud animal u otras actividades especiales, las condiciones adicionales pueden incluir requisitos de participación, términos de inscripción y detalles específicos del evento.</h5>
+          <h5><strong>Actualizaciones de Servicios</strong></h5>
+          <h5>En situaciones en las que se introduzcan nuevas características o mejoras en los servicios, pueden aplicarse condiciones adicionales para explicar cómo afectan estas actualizaciones a los propietarios.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Envíos</h3>
+          <h5>Al momento la Clínica Veterinaria Jireh no cuenta con un vínculo directo de alguna compañía de envíos, si no que, los envíos se tratan directamente desde la Clínica Veterinaria Jireh para hacer llegar el producto a los propietarios o en caso con algún repartidor de confianza. El costo del envió depende directamente de acorde al domicilio del propietario, el valor de consumo y puntos de fidelidad.</h5>
+        </div>
+
+        <div style={{marginBottom: "50px"}}>
+          <h3 style={{color: "#00A5cb"}}>Descuentos</h3>
+          <h5>Los descuentos se establecen directamente en anuncios dentro del sitio web o al seleccionar un producto y en la Clínica Veterinaria Jireh se da a conocer estos descuentos. En algunos productos se establecer un límite de productos ya sea que para que aplique el descuento debe comprar más de 2 productos o en algunos no deberá comprar más de 3 productos.</h5>
+        </div>
+
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+          <h5 style={{color: "#00A5cb"}}>Jireh se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.</h5>
+          <h5 style={{color: "#00A5cb"}}>Última actualización: 14/11/2023</h5>
+        </div>
+      
       </section>
+
+      <Footer />
 
     </div>
   )
